4cc027be463f8c23776469afb416b4b7ec76bb9f
thdaudwlswkd-eng <thdaudwlswkd@gmail.com>
2026-07-01 18:03:00 +0900

    update

--- push-to-github.bat
+++ push-to-github.bat
@@ -1,9 +1,11 @@
 @echo off
 cd /d "%~dp0"
+git config user.email "thdaudwlswkd@gmail.com"
+git config user.name "thdaudwlswkd-eng"
 git add -A
 git commit -m "update"
 git remote set-url origin https://thdaudwlswkd-eng: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@github.com/thdaudwlswkd-eng/tok-hanjang.git
 git push origin main
 echo.
-echo Done! Check above for errors.
+echo Done!
 pause
