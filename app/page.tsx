f2e86c5898aa99878c8b2d4cd7dbb41484122864
thdaudwlswkd-eng <thdaudwlswkd@gmail.com>
2026-07-02 08:18:16 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from 'next/navigation'
 import LandingPage from './LandingPage'
 
 export default function HomePage() {
-  const cardId = process.env.HOME_CARD_ID
+  const cardId = process.env.HOME_CARD_ID ?? process.env.HOME_D_ID
   if (cardId) redirect(`/card/${cardId}`)
   return <LandingPage />
 }
