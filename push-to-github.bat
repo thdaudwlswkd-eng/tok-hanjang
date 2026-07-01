3a6b29ac2675cbe8463be9a8135d933094a78045
thdaudwlswkd-eng <thdaudwlswkd@gmail.com>
2026-07-01 18:32:25 +0900

    update

--- push-to-github.bat
+++ push-to-github.bat
@@ -4,7 +4,7 @@ git config user.email "thdaudwlswkd@gmail.com"
 git config user.name "thdaudwlswkd-eng"
 git add -A
 git commit -m "update"
-git remote set-url origin https://thdaudwlswkd-eng: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@github.com/thdaudwlswkd-eng/tok-hanjang.git
+git remote set-url origin https://thdaudwlswkd-eng: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@github.com/thdaudwlswkd-eng/tok-hanjang.git
 git push origin main
 echo.
 echo Done!
